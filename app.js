5d8dfe6b301d1e5f7af6bb86d96f09f2f6b577f8
Will Kaspar <Will@Aang.local>
2017-02-02 22:52:53 -0800

    2/2/17 - full functionality

--- app.js
+++ app.js
@@ -3,39 +3,50 @@ console.log('js, you cray');
 
 $(document).ready(function(){
   // Grab current screen width
-  var screenWidth = 1000 //$(window).width();
+  var screenWidth = ($(window).width()-200);
   console.log('screenWidth '+screenWidth);
   // Initalize Player One's Car Object
   var pOne = {
-    top:  $('#pOne').animate().top,
+    //top:  $('#pOne').animate().top,
     left: $('#pOne').animate().left,
-    position: $('#pOne').position().left,
+    initalPos: $('#pOne').position().left,
     keyCode:  65
   }
   // Initalize Player Two's car
   var pTwo = {
-    top:  $('#pTwo').animate().top,
+    //top:  $('#pTwo').animate().top,
     left: $('#pTwo').animate().left,
-    position: $('#pTwo').position().left,
+    initalPos: $('#pTwo').position().left,
     keyCode:  76
   }
-
   // KeyDown To Move Functionality
   $(document).keydown(function(event){
-
-    // Set Win Conditions
-
-
     // Move Player One
     if(event.keyCode == pOne.keyCode){
-      console.log('p1 pressed '+pOne.keyCode);
       $("#pOne").animate({left : "+=65px"}, 400);
+      console.log('p1 position: '+$('#pOne').position().left);
     }
     // Move Player Two
     if(event.keyCode == pTwo.keyCode){
-      console.log('p2 pressed '+pTwo.keyCode);
       $("#pTwo").animate({left : "+=65px"}, 400);
+      console.log('p2 position: '+$('#pTwo').position().left);
     }
-
+    // Set Win Conditions
+    if (($('#pOne').position().left >= screenWidth) &&
+        ($('#pTwo').position().left >= screenWidth)){
+          alert('it is a tie!');
+          window.location.reload('true');
+      } else if($('#pOne').position().left >= screenWidth){
+        alert('player one wins!');
+        window.location.reload('true');
+      } else if ($('#pTwo').position().left >= screenWidth) {
+      alert('player two wins!');
+      window.location.reload('true');
+    }
+    // Start A New Game Function
+    var startNewGame = function(){
+      pOne.initalPos = 0;
+      pTwo.initalPos = 0;
+    }
   });
 });
